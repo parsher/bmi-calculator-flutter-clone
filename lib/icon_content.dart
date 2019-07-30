5f17f6bc85e6c06464dd9fc7bc1dde573e6024dd
parsher <futureslabor@gmail.com>
2019-07-31 08:25:40 +0900

    https://www.appbrewery.co course practice - BMI Calculator project

---
+++ lib/icon_content.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+import 'constants.dart';
+
+class IconContent extends StatelessWidget {
+  final IconData icon;
+  final String label;
+
+  IconContent({this.icon, this.label});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Icon(this.icon, size: 80.0),
+        SizedBox(height: 15.0),
+        Text(
+          this.label,
+          style: kLabelTextStyle,
+        )
+      ],
+    );
+  }
+}
